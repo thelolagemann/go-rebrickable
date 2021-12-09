6ffdd6e68eccfcc9cb94cb2f59e27910c194a253
Harry Bond <15960568+thelolagemann@users.noreply.github.com>
2021-12-09 05:43:38 +0000

    improve test handling

--- lego_test.go
+++ lego_test.go
@@ -1,82 +1,18 @@
 package rebrickable
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"net/http"
-	"os"
-	"strings"
 	"testing"
 )
 
-var (
-	globalMock = &mockClient{
-		mockResponse,
-	}
-	mockResponse = func(req *http.Request) (*http.Response, error) {
-		path := strings.Replace(req.URL.Path, "/api/v3/lego/", "", -1)
-		r := ioutil.NopCloser(bytes.NewReader(testData[path]))
-
-		return &http.Response{
-			StatusCode: 200,
-			Header: http.Header{
-				"Content-Type": []string{"application/json"},
-			},
-			Body: r,
-		}, nil
-	}
-	mockJSONSyntaxError = func(req *http.Request) (*http.Response, error) {
-		badJSON := `[{"response": beans"}, {"jem': "orange"}`
-		r := ioutil.NopCloser(strings.NewReader(badJSON))
-
-		return &http.Response{
-			StatusCode: 200,
-			Header: http.Header{
-				"Content-Type": []string{"application/json"},
-			},
-			Body: r,
-		}, nil
-	}
-	client   = NewLEGOClient("API_KEY", HTTPClient(globalMock))
-	testData map[string]json.RawMessage
-)
-
-func init() {
-	testFile, err := os.Open("testdata/responses.json")
-	if err != nil {
-		panic(err)
-	}
-	defer testFile.Close()
-	bytes, err := io.ReadAll(testFile)
-	if err != nil {
-		panic(err)
-	}
-	if err := json.Unmarshal(bytes, &testData); err != nil {
-		panic(err)
-	}
-}
-
-type mockDoFunc func(req *http.Request) (*http.Response, error)
-
-type mockClient struct {
-	mockDo mockDoFunc
-}
-
-func (m *mockClient) Do(req *http.Request) (*http.Response, error) {
-	return m.mockDo(req)
-}
-
 func TestLClient_Colors(t *testing.T) {
 	t.Run("Colors", func(t *testing.T) {
-		if _, err := client.Colors(); err != nil {
+		if _, err := legoClient.Colors(); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("Color", func(t *testing.T) {
-		if color, err := client.Color(212); err != nil {
+		if color, err := legoClient.Color(212); err != nil {
 			t.Error(err)
 		} else {
 			fmt.Println(color.Name)
@@ -85,30 +21,30 @@ func TestLClient_Colors(t *testing.T) {
 }
 
 func TestLClient_Element(t *testing.T) {
-	if _, err := client.Element("6143875"); err != nil {
+	if _, err := legoClient.Element("6143875"); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestLClient_Minifigs(t *testing.T) {
 	t.Run("Minifigs", func(t *testing.T) {
-		if _, err := client.Minifigs(); err != nil {
+		if _, err := legoClient.Minifigs(); err != nil {
 			t.Error(err)
 		}
 	})
 	setNumber := "fig-000003"
 	t.Run("Minifig", func(t *testing.T) {
-		if _, err := client.Minifig(setNumber); err != nil {
+		if _, err := legoClient.Minifig(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("MinifigParts", func(t *testing.T) {
-		if _, err := client.MinifigParts(setNumber); err != nil {
+		if _, err := legoClient.MinifigParts(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("MinifigSets", func(t *testing.T) {
-		if _, err := client.MinifigSets(setNumber); err != nil {
+		if _, err := legoClient.MinifigSets(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
@@ -116,12 +52,12 @@ func TestLClient_Minifigs(t *testing.T) {
 
 func TestLClient_PartCategories(t *testing.T) {
 	t.Run("PartCategories", func(t *testing.T) {
-		if _, err := client.PartCategories(); err != nil {
+		if _, err := legoClient.PartCategories(); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("PartCategory", func(t *testing.T) {
-		if _, err := client.PartCategory(3); err != nil {
+		if _, err := legoClient.PartCategory(3); err != nil {
 			t.Error(err)
 		}
 	})
@@ -129,29 +65,29 @@ func TestLClient_PartCategories(t *testing.T) {
 
 func TestLClient_Parts(t *testing.T) {
 	t.Run("Parts", func(t *testing.T) {
-		if _, err := client.Parts(); err != nil {
+		if _, err := legoClient.Parts(); err != nil {
 			t.Error(err)
 		}
 	})
 	partNumber := "15104"
 	colorNumber := 182
 	t.Run("Part", func(t *testing.T) {
-		if _, err := client.Part(partNumber); err != nil {
+		if _, err := legoClient.Part(partNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("PartColors", func(t *testing.T) {
-		if _, err := client.PartColors(partNumber); err != nil {
+		if _, err := legoClient.PartColors(partNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("PartColor", func(t *testing.T) {
-		if _, err := client.PartColor(partNumber, colorNumber); err != nil {
+		if _, err := legoClient.PartColor(partNumber, colorNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("PartColorSets", func(t *testing.T) {
-		if _, err := client.PartColorSets(partNumber, colorNumber); err != nil {
+		if _, err := legoClient.PartColorSets(partNumber, colorNumber); err != nil {
 			t.Error(err)
 		}
 	})
@@ -159,33 +95,33 @@ func TestLClient_Parts(t *testing.T) {
 
 func TestLClient_Sets(t *testing.T) {
 	t.Run("Sets", func(t *testing.T) {
-		if _, err := client.Sets(); err != nil {
+		if _, err := legoClient.Sets(); err != nil {
 			t.Error(err)
 		}
 	})
 	setNumber := "42102-1"
 	t.Run("Set", func(t *testing.T) {
-		if _, err := client.Set(setNumber); err != nil {
+		if _, err := legoClient.Set(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("SetAlternates", func(t *testing.T) {
-		if _, err := client.SetAlternates(setNumber); err != nil {
+		if _, err := legoClient.SetAlternates(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("SetMinifigs", func(t *testing.T) {
-		if _, err := client.SetMinifigs("7018-1"); err != nil {
+		if _, err := legoClient.SetMinifigs("7018-1"); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("SetParts", func(t *testing.T) {
-		if _, err := client.SetParts(setNumber); err != nil {
+		if _, err := legoClient.SetParts(setNumber); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("SetSets", func(t *testing.T) {
-		if _, err := client.SetSets("65757-1"); err != nil {
+		if _, err := legoClient.SetSets("65757-1"); err != nil {
 			t.Error(err)
 		}
 	})
@@ -193,12 +129,12 @@ func TestLClient_Sets(t *testing.T) {
 
 func TestLClient_Themes(t *testing.T) {
 	t.Run("Themes", func(t *testing.T) {
-		if _, err := client.Themes(); err != nil {
+		if _, err := legoClient.Themes(); err != nil {
 			t.Error(err)
 		}
 	})
 	t.Run("Theme", func(t *testing.T) {
-		if _, err := client.Theme(3); err != nil {
+		if _, err := legoClient.Theme(3); err != nil {
 			t.Error(err)
 		}
 	})
---
+++ rebrickable_test.go
@@ -0,0 +1,96 @@
+package rebrickable
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	globalMock = &mockClient{
+		mockResponse,
+	}
+	mockResponse = func(req *http.Request) (*http.Response, error) {
+		path := strings.Replace(req.URL.Path, "/api/v3/", "", -1)
+		var data json.RawMessage
+		if strings.Contains(path, "lego") {
+			data = tData.LEGO[strings.Replace(path, "lego/", "", -1)]
+		} else if strings.Contains(path, "users") {
+			// remove token
+			data = tData.Users[strings.SplitN(path, "/", 3)[2]]
+		} else {
+			panic("unhandled mock path")
+		}
+		r := ioutil.NopCloser(bytes.NewReader(data))
+
+		return &http.Response{
+			StatusCode: 200,
+			Header: http.Header{
+				"Content-Type": []string{"application/json"},
+			},
+			Body: r,
+		}, nil
+	}
+	client     = NewClient("", HTTPClient(globalMock))
+	legoClient = NewLEGOClient("API_KEY", HTTPClient(globalMock))
+	tData      testData
+)
+
+type testData struct {
+	LEGO  map[string]json.RawMessage `json:"lego"`
+	Users map[string]json.RawMessage `json:"users"`
+}
+
+func init() {
+	testFile, err := os.Open("testdata/responses.json")
+	if err != nil {
+		panic(err)
+	}
+	defer testFile.Close()
+	bytes, err := io.ReadAll(testFile)
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(bytes, &tData); err != nil {
+		panic(err)
+	}
+}
+
+type mockDoFunc func(req *http.Request) (*http.Response, error)
+
+type mockClient struct {
+	mockDo mockDoFunc
+}
+
+func (m *mockClient) Do(req *http.Request) (*http.Response, error) {
+	return m.mockDo(req)
+}
+
+func (m *mockClient) bypassMock(f func()) {
+	client := &http.Client{}
+	m.mockDo = client.Do
+	f()
+	m.mockDo = mockResponse
+}
+
+func (m *mockClient) mockResponse(statusCode int, body []byte, f func()) error {
+	r := &http.Response{
+		StatusCode: statusCode,
+		Header: http.Header{
+			"Content-Type": []string{"application/json"},
+		},
+		Body: ioutil.NopCloser(bytes.NewReader(body)),
+	}
+
+	m.mockDo = func(req *http.Request) (*http.Response, error) {
+		return r, nil
+	}
+	f()
+	m.mockDo = mockResponse
+
+	return nil
+}
